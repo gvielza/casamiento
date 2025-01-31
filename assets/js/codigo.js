9cfc6bc0ed65cac0d77ce434c6c94e65dd85e54e
unknown <voltagnelly@gmail.com>
2025-01-31 20:35:26 -0300

    contador

--- assets/js/codigo.js
+++ assets/js/codigo.js
@@ -27,3 +27,11 @@ function createHeart() {
 
 // Generar corazones cada 300ms
 setInterval(createHeart, 300);
+
+
+//counter
+(function(d, s, id) {
+   var js, pjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) return; js = d.createElement(s);
+    js.id = id; js.src = "//www.tickcounter.com/static/js/loader.js"; pjs.parentNode.insertBefore(js, pjs); }
+    (document, "script", "tickcounter-sdk"));
