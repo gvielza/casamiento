f6e35ef50e54c60f65668152b4f18792b5df4cad
unknown <voltagnelly@gmail.com>
2025-01-31 22:18:17 -0300

    ajustes

--- assets/js/codigo.js
+++ assets/js/codigo.js
@@ -32,6 +32,5 @@ setInterval(createHeart, 300);
 //counter
 (function(d, s, id) {
    var js, pjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) return; js = d.createElement(s);
-    js.id = id; js.src = "//www.tickcounter.com/static/js/loader.js"; pjs.parentNode.insertBefore(js, pjs); }
-    (document, "script", "tickcounter-sdk"));
+    if (d.getElementById(id)) return; js = d.createElement(s);
+     js.id = id; js.src = "//www.tickcounter.com/static/js/loader.js"; pjs.parentNode.insertBefore(js, pjs); }(document, "script", "tickcounter-sdk"));
